f8a4f96411194b3a4f7f636c724e7e93d0245f97
kuluruvineeth <kuluruvineeth8623@gmail.com>
2021-12-30 10:09:26 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxWigModule } from 'ngx-wig';
 
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgxWigModule
   ],
   providers: [],
   bootstrap: [AppComponent]
